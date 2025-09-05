f5057294eb40305aff8879f017f609be7f8ae20a
Jess <shawnisun@gmail.com>
2025-09-06 00:40:19 +0800

    middle

--- go.mod
+++ go.mod
@@ -8,7 +8,10 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -18,6 +21,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
+	gorm.io/gorm v1.30.3 // indirect
 )
 
 require (
---
+++ pkg/bvmiddleware/casbin.go
@@ -0,0 +1,35 @@
+package bvmiddleware
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cast"
+	"github.com/spf13/viper"
+)
+
+func Casbin() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if strings.ToLower(ctx.Request.Method) == "OPTIONS" {
+			ctx.Next()
+			return
+		}
+
+		roleId := gzauth.GetTokenValue[int64](ctx, "role_id")
+		if roleId == 0 {
+			gzconsole.Echo.Info("ℹ️ 提示: 无法使用 `Casbin` 权限校验, 请确保 `Token` 中包含了字段 `role_id`")
+			ctx.Next()
+			return
+		}
+
+		path := gzutil.ConvertToRestfulURL(strings.TrimPrefix(ctx.Request.URL.Path, viper.GetString("App.RouterPrefix")))
+		success, _ := gooze.Casbin.Enforce(cast.ToString(roleId), path, ctx.Request.Method)
+		if !success {
+			gooze.Fail(ctx, gzerror.NoAuth)
+			ctx.Abort()
+			return
+		}
+
+		ctx.Next()
+	}
+}
